377f0c3d1de0761fdd08778251dcb429297a9cac
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-07-08 18:24:26 -0700

    Create 20190708164737_jokes_table.js

---
+++ database/migrations/20190708164737_jokes_table.js
@@ -0,0 +1,23 @@
+exports.up = function(knex, Promise) {
+    return knex.schema.createTable('jokes',
+      table => {
+        table.increments();
+
+        table
+            .string('question')
+            .notNullable()
+            .unique()
+        table
+            .string('answer')
+            .notNullable()
+            .unique()
+        table
+            .boolean('public')
+            .notNullable()
+            .defaultTo(true);
+      })
+  };
+  
+exports.down = function(knex, Promise) {
+    return knex.schema.dropTableIfExists('jokes')
+};
