47284d33c89b0af3ca36eb1849c60db6a45f4da5
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-07-08 18:24:33 -0700

    Create auth-model.js

---
+++ models/auth-model.js
@@ -0,0 +1,14 @@
+const db = require('../database/dbConfig.js');
+
+module.exports = {
+    register: (newUser) => {
+        return db('users')
+            .insert(newUser)
+    },
+    login: (username) => {
+        return db('users')
+            .where({ username })
+            .first()
+    },
+}
+
