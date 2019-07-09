8beed0a60f5549c4243d6fb79c57b68b6ebf31e5
Mylynh Nguyen <34496865+nmylynh@users.noreply.github.com>
2019-07-08 18:23:38 -0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const server = express();
+const configureMiddleware = require('./middleware/config-mw');
+const auth = require('./routers/auth-router');
+const users = require('./routers/users-router');
+const jokes = require('./routers/jokes-router');
+
+configureMiddleware(server);
+
+server.get('/', (req, res) => {
+    res.send(`<h2>Welcome to DAD JOKES!</h2>`)
+  });  
+
+
+server.use('/api/auth', auth);
+server.use('/api/users', users);
+server.use('/api/jokes', jokes)
+
+
+module.exports = server;
